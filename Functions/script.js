1232696d43eba80cc8b8205b68f016a293351c59
Akshat Bhushan <ak2111bh@gmail.com>
2026-04-24 00:28:01 +0530

    first function

---
+++ Functions/script.js
@@ -0,0 +1,22 @@
+// 🏁 Trials 
+// function bro () {
+//   let name= prompt ("Enter name")
+//   console.log(`Hey ${name} bro, Sup!`)
+// }
+
+// bro ()
+
+// function trial () {
+//     console.log("1.This is function trial part")
+//     console.log("This is function trial part, okay?")
+//     console.log("This is function trial part, Understood?")
+//     console.log("L.This is function trial part, fine then!")
+// }
+
+// trial()
+
+// let fn = function () {
+//     console.log("fn is the name of this function here")
+// }
+
+// fn();
